de190451055d05452b6755a40a743349338af365
RussellCarey <62222824+RussellCarey@users.noreply.github.com>
2022-07-09 09:21:00 +0900

    Added edge detection

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
 var imgIn;
 
-// Kernal / maxtrix
-var matrix = [
-    [1/9, 1/9, 1/9],
-    [1/9, 1/9, 1/9],
-    [1/9, 1/9, 1/9],
+var matrixX = [
+    [-1. -2. -1],
+    [0, 0, 0],
+    [1, 2, 1],
+]
+
+var matrixY = [
+    [-1, 0, 1],
+    [-2, 0, 2],
+    [-1, 0, 1],
 ]
 
 function preload() {
@@ -18,6 +23,7 @@ function setup() {
 }
 
 function draw() {
+    imgIn.filter(GRAY);
     image(imgIn, 0, 0);
     image(blurFilter(imgIn), imgIn.width + 20, 1);
     noLoop();
@@ -26,22 +32,28 @@ function draw() {
 function blurFilter(img) {
     var imgOut = createImage(img.width, img.height);
     // Matrix size
-    var matrixSize = matrix.length;
+    var matrixSize = matrixX.length;
 
     imgOut.loadPixels();
     img.loadPixels();
 
     for(var x=0; x<img.width; x++) {
         for(var y=0; y<img.height; y++) {
-            //
             var index = (y*img.width + x) * 4;
 
-            // Convolution function
-            var con = convolution(x, y, matrix, matrixSize, imgIn);
+            // Two calls for X and Y direction.
+            var cX =  convolution(x, y, matrixX, matrixSize, imgIn);
+            var cY =  convolution(x, y, matrixY, matrixSize, imgIn);
 
-            imgOut.pixels[index + 0] = con[0];
-            imgOut.pixels[index + 1] = con[1];
-            imgOut.pixels[index + 2] = con[2];
+            // 1 + 2 + 1 * 255 = 1020 // Max val of the con function
+            cX = map(abs(cX[0]), 0, 1020, 0, 255);
+            cY = map(abs(cX[0]), 0, 1020, 0, 255);
+
+            var combine = cX + cY;
+
+            imgOut.pixels[index + 0] = combine;
+            imgOut.pixels[index + 1] = combine;
+            imgOut.pixels[index + 2] = combine;
             imgOut.pixels[index + 3] = 255;
         }
     }
@@ -66,6 +78,7 @@ function convolution(x, y, matrix, matrixSize, img) {
             var yLoc = y + j - offset;
 
             var index = (img.width * yLoc + xLoc) * 4;
+            
             // Check it is actually inside the pixels.
             index = constrain(index, 0, img.pixels.length - 1);
 
